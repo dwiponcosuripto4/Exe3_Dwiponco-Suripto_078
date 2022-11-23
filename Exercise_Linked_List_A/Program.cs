205abcd3e35e147faf8ce7159b11209a0b68af47
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2022-11-23 15:50:15 +0700

    membuat method mengembalikan nilai di search

--- Exercise_Linked_List_A/Program.cs
+++ Exercise_Linked_List_A/Program.cs
@@ -24,7 +24,14 @@ namespace Exercise_Linked_List_A
 
         public bool Search(int rollNo, ref Node previous, ref Node current)/*Searches for the specified node*/
         {
-            for (previous = current = LAST.next)
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (previous = current = current.next)
+                {
+                    if (rollNo == current.rollNumber)
+                        return (true);/*returns true if the node is found*/
+                }
+            }
         }
     }
 }
