037196243fd410358e461ff457dd44c3ea5bfa9b
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2022-11-23 15:45:33 +0700

    menambah variabel kelas node

--- Exercise_Linked_List_A/Program.cs
+++ Exercise_Linked_List_A/Program.cs
@@ -13,4 +13,18 @@ namespace Exercise_Linked_List_A
         public string name;
         public Node next;
     }
+    class CircularList
+    {
+        Node LAST;
+
+        public CircularList()
+        {
+            LAST = null;
+        }
+
+        public bool Search(int rollNo, ref Node previous, ref Node current)/*Searches for the specified node*/
+        {
+            for (previous = current = LAST.next)
+        }
+    }
 }
