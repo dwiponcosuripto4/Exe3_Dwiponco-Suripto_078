aa3d8b6f0d750e38687536a5a6fdd2a7f58dfbd6
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2022-11-23 15:37:18 +0700

    Add project files.

---
+++ Exercise_Linked_List_A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Linked_List_A
+{
+    class Node
+    {
+        /*create Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+}
